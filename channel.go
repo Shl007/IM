474aef4e9f391b608ca20341ab1265f3a64d905d
Shl007 <1031652416@qq.com>
2022-12-21 22:58:47 +0800

    feat: 通信层-websocket

--- channel.go
+++ channel.go
@@ -1,6 +1,11 @@
 package im
 
-import "time"
+import (
+	"errors"
+	"im/logger"
+	"sync"
+	"time"
+)
 
 // Channel is interface of client side
 type Channel interface {
@@ -12,4 +17,138 @@ type Channel interface {
 	// SetWriteWait 设置写超时
 	SetWriteWait(time.Duration)
 	SetReadWait(time.Duration)
+}
+
+// ChannelImpl is a websocket implement of channel
+type ChannelImpl struct {
+	sync.Mutex
+	id string
+	Conn
+	writechan chan []byte
+	once      sync.Once
+	writeWait time.Duration
+	readwait  time.Duration
+	closed    *Event
+}
+
+// NewChannel NewChannel
+func NewChannel(id string, conn Conn) Channel {
+	log := logger.WithFields(logger.Fields{
+		"module": "channel",
+		"id":     id,
+	})
+	ch := &ChannelImpl{
+		id:        id,
+		Conn:      conn,
+		writechan: make(chan []byte, 5),
+		closed:    NewEvent(),
+		writeWait: DefaultWriteWait, //default value
+		readwait:  DefaultReadWait,
+	}
+	go func() {
+		err := ch.writeloop()
+		if err != nil {
+			log.Info(err)
+		}
+	}()
+	return ch
+}
+
+func (ch *ChannelImpl) writeloop() error {
+	for {
+		select {
+		case payload := <-ch.writechan:
+			err := ch.WriteFrame(OpBinary, payload)
+			if err != nil {
+				return err
+			}
+			chanlen := len(ch.writechan)
+			for i := 0; i < chanlen; i++ {
+				payload = <-ch.writechan
+				err := ch.WriteFrame(OpBinary, payload)
+				if err != nil {
+					return err
+				}
+			}
+			err = ch.Conn.Flush()
+			if err != nil {
+				return err
+			}
+		case <-ch.closed.Done():
+			return nil
+		}
+	}
+}
+
+// ID id
+func (ch *ChannelImpl) ID() string { return ch.id }
+
+func (ch *ChannelImpl) Push(payload []byte) error {
+	if ch.closed.HasFired() {
+		return errors.New("channel has closed")
+	}
+	// 异步写
+	ch.writechan <- payload
+	return nil
+}
+
+// overwrite Conn
+func (ch *ChannelImpl) WriteFrame(code OpCode, payload []byte) error {
+	_ = ch.Conn.SetWriteDeadline(time.Now().Add(ch.writeWait))
+	return ch.Conn.WriteFrame(code, payload)
+}
+
+// Close 关闭连接
+func (ch *ChannelImpl) Close() error {
+	ch.once.Do(func() {
+		close(ch.writechan)
+		ch.closed.Fire()
+	})
+	return nil
+}
+
+// SetWriteWait 设置写超时
+func (ch *ChannelImpl) SetWriteWait(writeWait time.Duration) {
+	if writeWait == 0 {
+		return
+	}
+	ch.writeWait = writeWait
+}
+
+func (ch *ChannelImpl) SetReadWait(readwait time.Duration) {
+	if readwait == 0 {
+		return
+	}
+	ch.writeWait = readwait
+}
+
+func (ch *ChannelImpl) Readloop(lst MessageListener) error {
+	ch.Lock()
+	defer ch.Unlock()
+	log := logger.WithFields(logger.Fields{
+		"struct": "ChannelImpl",
+		"func":   "Readloop",
+		"id":     ch.id,
+	})
+	for {
+		_ = ch.SetReadDeadline(time.Now().Add(ch.readwait))
+
+		frame, err := ch.ReadFrame()
+		if err != nil {
+			return err
+		}
+		if frame.GetOpCode() == OpClose {
+			return errors.New("remote side close the channel")
+		}
+		if frame.GetOpCode() == OpPing {
+			log.Trace("recv a ping; resp with a pong")
+			_ = ch.WriteFrame(OpPong, nil)
+			continue
+		}
+		payload := frame.GetPayload()
+		if len(payload) == 0 {
+			continue
+		}
+		go lst.Receive(ch, payload)
+	}
 }
---
+++ websocket/connection.go
@@ -0,0 +1,59 @@
+package websocket
+
+import (
+	"github.com/gobwas/ws"
+	"im"
+	"net"
+)
+
+type Frame struct {
+	raw ws.Frame
+}
+
+func (f *Frame) SetOpCode(code im.OpCode) {
+	f.raw.Header.OpCode = ws.OpCode(code)
+}
+
+func (f *Frame) GetOpCode() im.OpCode {
+	return im.OpCode(f.raw.Header.OpCode)
+}
+
+func (f *Frame) SetPayload(payload []byte) {
+	f.raw.Payload = payload
+}
+
+func (f *Frame) GetPayload() []byte {
+	// 表示是否要对数据载荷进行掩码操作。从客户端向服务端发送数据时，需要对数据进行掩码操作；从服务端向客户端发送数据时，不需要对数据进行掩码操作
+	if f.raw.Header.Masked {
+		ws.Cipher(f.raw.Payload, f.raw.Header.Mask, 0)
+	}
+	f.raw.Header.Masked = false
+	return f.raw.Payload
+}
+
+type WsConn struct {
+	net.Conn
+}
+
+func NewConn(conn net.Conn) *WsConn {
+	return &WsConn{
+		Conn: conn,
+	}
+}
+
+func (c *WsConn) ReadFrame() (im.Frame, error) {
+	f, err := ws.ReadFrame(c.Conn)
+	if err != nil {
+		return nil, err
+	}
+	return &Frame{raw: f}, nil
+}
+
+func (c *WsConn) WriteFrame(code im.OpCode, payload []byte) error {
+	f := ws.NewFrame(ws.OpCode(code), true, payload)
+	return ws.WriteFrame(c.Conn, f)
+}
+
+func (c *WsConn) Flush() error {
+	return nil
+}
